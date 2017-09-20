6ce584a45a8e4792c264ed3f1dd98029b6efab96
The Punisher <bazz455.bs@gmail.com>
2017-09-20 12:13:53 +0500

    configured Socialite

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Socialite;
 class LoginController extends Controller
 {
     /*
@@ -36,4 +36,22 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function redirectToProvider()
+  {
+      return Socialite::driver('github')->redirect();
+  }
+
+  /**
+   * Obtain the user information from GitHub.
+   *
+   * @return Response
+   */
+  public function handleProviderCallback()
+  {
+      $user = Socialite::driver('github')->user();
+
+      // $user->token;
+  }
+
 }
